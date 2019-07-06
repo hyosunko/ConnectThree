b4f6720ee58e9ad0813bd1070f01b183cd656c86
hyosunko <hyosunko@gmail.com>
2019-07-06 15:02:54 -0700

    add turn & score

--- app/src/androidTest/java/com/hyosunko/connectthree/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hyosunko/connectthree/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.connectthree;
+package com.hyosunko.connectthree;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/com/hyosunko/connectthree/ExampleUnitTest.java
+++ app/src/test/java/com/hyosunko/connectthree/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.connectthree;
+package com.hyosunko.connectthree;
 
 import org.junit.Test;
 
